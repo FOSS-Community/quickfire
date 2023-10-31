ae28194c4c77000ab5727e4acd9ffd052e852ad9
utkarsh <utkarsh@debian.debian.com>
2023-10-31 22:25:13 +0530

    Initial commit

---
+++ bin/quickfire.dart
@@ -0,0 +1,5 @@
+import 'package:quickfire/quickfire.dart' as quickfire;
+
+void main(List<String> arguments) {
+  print('Hello world: ${quickfire.calculate()}!');
+}
---
+++ test/quickfire_test.dart
@@ -0,0 +1,8 @@
+import 'package:quickfire/quickfire.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
